3eb7048edc5a4c77a2e86d4b492af7f626a7d5a0
notToBeNamed <43531263+notToBeNamed@users.noreply.github.com>
2018-09-30 14:08:49 -0500

    Update parseInput.js
    @Pratima - Some initial comments where you can improve.

--- parseInput.js
+++ parseInput.js
@@ -1,23 +1,23 @@
 const fs = require('fs');
-
+//Like I mentioned that day, reject and resolve are just function calls, you need to return the callback.
+//If you don't you will run into unecessary errors (hard to debug).
+//TODO : Your function that you are exporting is bad style of coding; rather define function and then export it.
 module.exports = (inputFile) => {
   return new Promise((resolve, reject) => {
     fs.readFile(inputFile, { encoding: 'utf8' }, (err, data) => {
       if(err) {
-        reject(err);
+        return reject(err);
       }
-
       var lines = data.split('\n');
-
       var input = {};
       input.n = lines[0];
       input.ids = lines[1].split(' ');
       input.graph = [];
+      //Use forEach whenever you are iterating via value and for(let iter in something) when you want to iterate via index.
       for(let i = 0; i < input.n; i++) {
         input.graph.push(lines[i + 2].split(' '));
       }
-
-      resolve(input);
+      return resolve(input);
     });
   });
 }
