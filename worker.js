3eaeffef921990ef819bd25b0d1d681cc95a93dd
Raakesh Madana <madanaraakesh@gmail.com>
2018-10-12 18:51:28 -0500

    Send NACK messages
    Each worker after selecting a parent sends NACKs to all the other workers from
    which it receives an EXPLORE message from.

--- worker.js
+++ worker.js
@@ -8,6 +8,7 @@ const source = process.argv[3] === 'true' ? true: false;
 var marked = source;
 var maxUid = uid;
 var parentWorker = null;
+var rejectedWorkers = [];
 
 const incomingConnections = [];
 const outgoingConnections = [];
@@ -39,23 +40,38 @@ process.on('message', (message) => {
 function floodmax() {
   let message = {
     source: process.pid,
-    maxUid: maxUid,
-    bfs: null
+    maxUid: maxUid
   };
   if (marked) {
-    message.bfs = messageTypes.EXPLORE;
+    outgoingConnections.forEach((outgoingConnection) => {
+      if (outgoingConnection.remotePort === parentWorker) {
+        message.bfs = null;
+      } else if (rejectedWorkers.includes(outgoingConnection.remotePort)) {
+        message.bfs = messageTypes.NACK;
+      } else {
+        message.bfs = messageTypes.EXPLORE;
+      }
+      outgoingConnection.write(JSON.stringify(message));
+    });
     marked = false;
+  } else {
+    outgoingConnections.forEach((outgoingConnection) => {
+      if (rejectedWorkers.includes(outgoingConnection.remotePort)) {
+        message.bfs = messageTypes.NACK;
+      } else {
+        message.bfs = null;
+      }
+      outgoingConnection.write(JSON.stringify(message));
+    });
   }
-  outgoingConnections.forEach((outgoingConnection) => {
-    outgoingConnection.write(JSON.stringify(message));
-  });
+  rejectedWorkers = [];
 }
 
 function initiateConnections({neighbors}) {
   setUpConnectionListener(neighbors.length);
   neighbors.forEach((neighbor) => {
     let socket = net.createConnection(neighbor, () => {
-      console.log(uid + ' connected to ' + neighbor);
+      //console.log(uid + ' connected to ' + neighbor);
     });
     outgoingConnections.push(socket);
   });
@@ -76,24 +92,48 @@ function listener() {
 function processMessages(messages) {
   let msgObjs = messages.map((message) => JSON.parse(message));
 
+  // Find Max UID
   let uids = msgObjs.map((msgObj) => msgObj.maxUid);
-  maxUid = Math.max(...uids, maxUid).toString();
+  maxUid = Math.max(...uids, maxUid);
+  //console.log(uid + ' has seen ' + uids + '; max is ' + maxUid);
 
-  let exploreRequests = [];
-  msgObjs.forEach((msgObj) => {
-    if (msgObj.bfs === messageTypes.EXPLORE) {
-      console.log(uid, msgObj.source, msgObj.bfs);
-      exploreRequests.push(msgObj.source);
-    }
+  let exploreRequests = msgObjs.filter((msgObj) => {
+    return msgObj.bfs === messageTypes.EXPLORE;
   });
 
-  if (exploreRequests.length && !parentWorker && !source) {
-    let randomIndex = Math.floor(Math.random() * exploreRequests.length);
-    parentWorker = exploreRequests[randomIndex];
-    marked = true;
-    console.log(uid, parentWorker);
+  exploreRequests = exploreRequests.map((exploreRequest) => {
+    return exploreRequest.source;
+  });
+
+  if (exploreRequests.length) {
+    //console.log(uid, exploreRequests);
+    // Select parent worker
+    if (!parentWorker && !source) {
+      let randomIndex = Math.floor(Math.random() * exploreRequests.length);
+      parentWorker = exploreRequests[randomIndex];
+      marked = true;
+    }
+    // Keep track of processes that were rejected to be parents
+    rejectedWorkers = exploreRequests.filter((exploreRequest) => {
+      return exploreRequest !== parentWorker;
+    });
+    //if(rejectedWorkers.length) {
+      console.log(uid, parentWorker, rejectedWorkers);
+    //}
   }
-  //console.log(uid + ' has seen ' + uids + '; max is ' + maxUid);
+
+  let nacks = msgObjs.filter((msgObj) => {
+    return msgObj.bfs === messageTypes.NACK;
+  });
+
+  nacks = nacks.map((nack) => {
+    return nack.source;
+  });
+
+  if (nacks.length) {
+    console.log(uid + ' nacks: ' + nacks);
+  }
+
   let payload = {
     terminated: false,
     leader: maxUid,
